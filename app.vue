ef1bd97f94a9b082217b11973772c2945be088b4
tech192 <36390917-tech192@users.noreply.replit.com>
2025-01-31 13:47:14 +0000

    Agent query: Is the basic Nuxt application structure working with the navigation bar visible?
    Migrate project to Nuxt 3 and update dependencies.

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <NavBar />
+    <main class="flex-1">
+      <NuxtPage />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
